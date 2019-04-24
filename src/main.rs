4481e4a4520c6084a8f85b792c4a9168d2b77abf
Markus Røyset <cosmicspacedragon@gmail.com>
2019-04-24 18:39:45 +0200

    extract the letter shifting into its own function

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,33 @@ use structopt::StructOpt;
 fn main() {
     let opts = Opts::from_args();
 
-    opts.words
+    println!("{}", flshift(&opts.words, opts.shift_amount as usize));
+}
+
+fn flshift(words: &[String], shift_amount: usize) -> String {
+    let mut out = words
         .iter()
         .zip(
-            opts.words
+            words
                 .iter()
                 .map(|word| word.chars().nth(0).unwrap())
                 .cycle()
-                .skip(opts.shift_amount as usize),
+                .skip(shift_amount),
         )
-        .map(|(word, letter)| format!("{}{}", letter, word.chars().skip(1).collect::<String>()))
-        .for_each(|word| print!("{} ", word));
+        .map(|(word, letter)| {
+            let mut word: String = word.chars().skip(1).collect();
+            word.insert(0, letter);
+            word
+        })
+        .fold(String::new(), |mut s, word| {
+            s.push_str(&word);
+            s.push(' ');
+            s
+        });
 
-    println!();
+    out.pop();
+
+    out
 }
 
 #[derive(StructOpt, Debug)]
